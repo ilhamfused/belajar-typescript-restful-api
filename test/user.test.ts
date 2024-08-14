11278909760e510d2683f69c62f2860fdceabbc2
ilhamfused <irharijanto20@gmail.com>
2024-08-14 09:52:12 +0700

    add user unit test

---
+++ test/user.test.ts
@@ -0,0 +1,36 @@
+import { app } from "../src/application/app";
+import supertest from "supertest";
+import { logger } from "../src/application/logging";
+import { UserTest } from "./test-util";
+
+describe("POST /api/users", () => {
+
+  afterEach(async() => {
+    await UserTest.delete()
+  })
+
+  it("should reject register new user if request is invalid", async () => {
+    const response = await supertest(app).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+    });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toBeDefined();
+  });
+
+  it("Should register new user", async () => {
+    const response = await supertest(app).post("/api/users").send({
+      username: "test",
+      password: "test",
+      name: "test",
+    });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.username).toBe("test");
+    expect(response.body.data.name).toBe("test");
+  });
+});
